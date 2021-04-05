bcd8c93d1425b2c2381f7f46dd6f932fde2316d3
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2021-04-05 17:58:01 +0300

    🎨 Move and improve the status system cuz why not

--- index/index.js
+++ index/index.js
@@ -33,3 +33,25 @@ function postData(data) {
             console.error('Error:', error);
         });
 }
+
+function checkStatus() {
+    let startTime = Date.now();
+    try {
+        fetch('./status_checking_file_lol').then((res) => {
+            document.getElementById('statusText').innerText =
+                `${res.status == 200 ?
+                    '✅ Yay! Seems like the server is up and running ' :
+                    '❌ Hmmm... Something\'s wrong with the server '
+                } `
+                +
+                `${res.status} Ping:${Date.now() - startTime}ms`;
+            clearInterval(waitingInterval)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    let waitingInterval = setInterval(() => {
+        document.getElementById('statusText').innerText = `🤔 Waiting for the server to respond ${Math.round((Date.now() - startTime) / 1000)} seconds`
+    }, 1000)
+}
