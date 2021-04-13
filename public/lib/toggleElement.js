c5b15662a2177bada9a795a23bf7fbe56c3dd7de
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2021-04-14 00:28:43 +0300

    ✨Added a cool new toggleable element& some styling

---
+++ public/lib/toggleElement.js
@@ -0,0 +1,45 @@
+/**
+ * An element that can be toggled on and off
+ */
+class ToggleableElement extends HTMLElement {
+
+    get state() { return this.getAttribute("state"); }
+
+    /**
+     * @param {"on"|"off"} val The state of the toggleable element
+     */
+    set state(val) {
+        if (this.getAttribute("state") !== val)
+            this.setAttribute("state", val === ("on" || "off") ? val : "off");
+        return this.getAttribute("state")
+    }
+
+    switchState() {
+        this.state = this.state === "on" ? "off" : "on";
+        return this.state
+    }
+
+    constructor() {
+        super();
+        if (this.getAttribute("state") !== ("on" || "off")) {
+            this.setAttribute("state", "off");
+        }
+        this.state = this.getAttribute("state");
+        this.onstatechange = () => { }
+
+        this.addEventListener("click", () => {
+            this.switchState();
+        })
+    }
+
+    static get observedAttributes() {
+        return ["state"];
+    }
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        if (attrName == "state" && oldVal != newVal) {
+            this.onstatechange();
+        }
+    }
+}
+
+customElements.define("toggleable-element", ToggleableElement);
