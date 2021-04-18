bf7443343a8d588e16b54f202219df93294469c4
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2021-04-18 18:42:23 +0300

    ➕Add html-clickable-element to lib

---
+++ public/lib/clickableElement.js
@@ -0,0 +1,57 @@
+/**
+ * An element that can be clicked (Button div lmao)
+ */
+class ClickableElement extends HTMLElement {
+    get state() { return this.getAttribute("state"); }
+
+    /**
+     * @param {"on"|"off"} val The state of the clickable element
+     */
+    set state(val) {
+        if (this.getAttribute("state") !== val)
+            this.setAttribute("state", val === ("on" || "off") ? val : "off");
+        return this.getAttribute("state")
+    }
+
+    constructor() {
+        super();
+        if (this.getAttribute("state") !== ("on" || "off")) {
+            this.setAttribute("state", "off");
+        }
+        this.state = this.getAttribute("state");
+        this.onstatechange = () => { }
+        this.onclickstart = () => { }
+        this.onclickend = () => { }
+
+        this.addEventListener("mousedown", () => {
+            this.state = "on";
+        });
+        this.addEventListener("touchstart", () => {
+            this.state = "on";
+        });
+        this.addEventListener("mouseup", () => {
+            this.state = "off";
+        });
+        this.addEventListener("touchend", () => {
+            this.state = "off";
+        });
+    }
+
+    static get observedAttributes() {
+        return ["state"];
+    }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        if (attrName == "state" && oldVal != newVal) {
+            this.onstatechange();
+            if (newVal == "on") {
+                this.onclickstart();
+            }
+            else if (newVal == "off") {
+                this.onclickend();
+            }
+        }
+    }
+}
+
+customElements.define("clickable-element", ClickableElement)
