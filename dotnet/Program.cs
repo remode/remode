a516eeb0d956fad67cde1b051c9d91374c57e5e8
creeperkafasi <49831545+creeperkafasi@users.noreply.github.com>
2021-07-08 01:28:18 +0300

    working SendInput in C#

---
+++ dotnet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+using static dotnet.Winapi;
+
+namespace dotnet
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            string emoji = "👌";
+
+            Input[] inputs = new Input[]
+            {
+                new Input
+                {
+                    type = (int)InputType.Keyboard,
+                    u = new InputUnion
+                    {
+                        ki = new KeyboardInput
+                        {
+                            wVk = 0x0,
+                            wScan = (ushort)emoji[0],
+                            dwFlags = (uint)(KeyEventF.KeyDown | KeyEventF.Unicode),
+                            dwExtraInfo = GetMessageExtraInfo()
+                        }
+                    }
+                }
+            };
+
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+
+            inputs[0].u.ki.wScan = (ushort)emoji[1];
+
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+        }
+
+    }
+}
